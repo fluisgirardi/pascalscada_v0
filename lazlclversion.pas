60fcdfc8bc9cd6e269ae72e0bb56012d8e43e475
Fabio Luis Girardi <papelhigienico@users.sourceforge.net>
2011-01-17 16:17:21 +0000

    ** Custom hints, step 3

---
+++ lazlclversion.pas
@@ -0,0 +1,35 @@
+unit lazlclversion;
+
+{$IFDEF FPC}
+{$MODE Delphi}
+{$ENDIF}
+
+interface
+
+{$IFDEF FPC}
+uses
+  LCLVersion;
+{$ENDIF}
+
+type
+
+  THasCustomHint = (hchNo,hchYes);
+
+  {$if declared(lcl_version)}
+    {$IF not declared(lcl_fullversion)}
+      const lcl_fullversion = ((lcl_major *  100 + lcl_minor) * 100 + lcl_release) * 100;
+    {$ifend}
+  {$else}
+    const lcl_fullversion = 0;
+  {$ifend}
+
+  {$if declared(lcl_fullversion)}
+    {$if lcl_fullversion>=092900}
+      const has_customhints = hchYes;
+    {$ifend}
+  {$ifend}
+
+implementation
+
+end.
+
