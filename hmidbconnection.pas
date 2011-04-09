9bebd27db5fcccd3eb3ebe6b56fb9b49200f51bf
Fabio Luis Girardi <papelhigienico@users.sourceforge.net>
2011-04-09 12:22:38 +0000

    ** HMIDBConnection

--- hmidbconnection.pas
+++ hmidbconnection.pas
@@ -8,7 +8,7 @@ interface
 
 uses
   Classes, sysutils, ZConnection, ZPropertyEditor, MessageSpool, CrossEvent,
-  syncobjs;
+  syncobjs, memds, ZDataset;
 
 type
   THMIDBDatabasePropertyEditor = class(TZDatabasePropertyEditor)
@@ -23,30 +23,52 @@ type
 
   THMIDBProtocolPropertyEditor = class(TZProtocolPropertyEditor);
 
+  TExecSQLProc = procedure(sqlcmd:String; outputdataset:TMemDataset) of object;
+
+  TReturnDataSetProc = procedure(Sender:TObject; DS:TMemDataset) of object;
+
   IHMIDBConnection = interface
     ['{C5AEA572-D7F8-4116-9A4B-3C3B972DC021}']
     function GetSyncConnection:TZConnection;
-    function ExecSQL(sql:String; NotifyAfter:TNotifyEvent):Integer;
+    function ExecSQL(sql:String; ReturnDatasetCallback:TReturnDataSetProc):Integer;
   end;
+
+  TSQLCmdRec = record
+    SQLCmd:String;
+    ReturnDataSetCallback:TReturnDataSetProc;
+  end;
+
+  PSQLCmdRec = ^TSQLCmdRec;
 
   TProcessSQLCommandThread=class(TCrossThread)
   private
     FSpool:TMessageSpool;
     FEnd:TCrossEvent;
+    cmd:PSQLCmdRec;
+    fds:TMemDataset;
+    fOnExecSQL:TExecSQLProc;
   protected
     procedure Execute; override;
+    procedure ReturnData;
   public
-    constructor Create(CreateSuspended: Boolean);
+    constructor Create(CreateSuspended: Boolean; ExecSQLProc:TExecSQLProc);
     destructor Destroy; override;
     function WaitEnd(Timeout:Cardinal):TWaitResult;
+  public
+    procedure ExecSQLWithoutResultSet(sql:String);
+    procedure ExecSQLWithResultSet(sql:String; ReturnDataCallback:TReturnDataSetProc);
   end;
 
   THMIDBConnection = class(TComponent, IHMIDBConnection)
   private
     FSyncConnection,
-    FASyncConneciton:TZConnection;
-    function GetSyncConnection:TZConnection;
-    function ExecSQL(sql:String; NotifyAfter:TNotifyEvent):Integer;
+    FASyncConnection:TZConnection;
+    FASyncQuery:TZQuery;
+    FCS:TCriticalSection;
+    FSQLSpooler:TProcessSQLCommandThread;
+    function  GetSyncConnection:TZConnection;
+    function  ExecSQL(sql:String; ReturnDatasetCallback:TReturnDataSetProc):Integer;
+    procedure ExecuteSQLCommand(sqlcmd:String; outputdataset:TMemDataset);
   protected
     FProtocol: string;
     FHostName: string;
@@ -58,6 +80,7 @@ type
   protected
     function GetConnected:Boolean;
 
+    procedure SetConnected(x:Boolean);
     procedure SetProtocol(x: String);
     procedure SetHostName(x: String);
     procedure SetPort(x: Integer);
@@ -65,7 +88,7 @@ type
     procedure SetUser(x: String);
     procedure SetPassword(x: String);
     procedure SetCatalog(x: String);
-    procedure SetConnected(x:Boolean);
+
   public
     constructor Create(AOwner: TComponent); override;
     destructor  Destroy; override;
@@ -79,6 +102,9 @@ type
     property Password: string  read FPassword    write SetPassword;
     property Catalog:  string  read FCatalog     write SetCatalog;
   end;
+
+const
+  SQLCommandMSG = 1024;
 
 implementation
 
@@ -104,24 +130,75 @@ end;
 //THREAD DE EXECUÇÃO DOS COMANDOS SQL THMIDBCONNECTION
 //##############################################################################
 
-constructor TProcessSQLCommandThread.Create(CreateSuspended: Boolean);
+constructor TProcessSQLCommandThread.Create(CreateSuspended: Boolean; ExecSQLProc:TExecSQLProc);
 begin
-
+  inherited Create(CreateSuspended);
+  FSpool:=TMessageSpool.Create;
+  FEnd:=TCrossEvent.Create(nil,true,false,'');
+  fOnExecSQL:=ExecSQLProc;
 end;
 
 destructor  TProcessSQLCommandThread.Destroy;
 begin
-
+  inherited Destroy;
+  FEnd.Destroy;
 end;
 
 procedure   TProcessSQLCommandThread.Execute;
+var
+  msg:TMSMsg;
 begin
+  FEnd.ResetEvent;
+  while not Terminated do begin
+    while FSpool.PeekMessage(msg,0,0,true) do begin
+      //executa o comando sql
+      if (msg.MsgID=SQLCommandMSG) and (msg.wParam<>nil) then begin
+        cmd:=PSQLCmdRec(msg.wParam);
+        try
+          //se é necessario retornar algo
+          //cria o dataset de retorno de dados.
+          if Assigned(cmd^.ReturnDataSetCallback) then
+            fds:=TMemDataset.Create(Nil)
+          else
+            fds:=nil;
 
+          if Assigned(fOnExecSQL) then
+            fOnExecSQL(cmd^.SQLCmd, fds);
+
+          if Assigned(cmd^.ReturnDataSetCallback) then
+            Synchronize(ReturnData);
+        finally
+          Dispose(cmd);
+        end;
+      end;
+    end;
+  end;
+  FEnd.SetEvent;
+end;
+
+procedure   TProcessSQLCommandThread.ReturnData;
+begin
+  cmd^.ReturnDataSetCallback(self,fds);
 end;
 
 function    TProcessSQLCommandThread.WaitEnd(Timeout:Cardinal):TWaitResult;
 begin
+  Result := FEnd.WaitFor(Timeout);
+end;
 
+procedure   TProcessSQLCommandThread.ExecSQLWithoutResultSet(sql:String);
+begin
+  ExecSQLWithResultSet(sql,nil);
+end;
+
+procedure   TProcessSQLCommandThread.ExecSQLWithResultSet(sql:String; ReturnDataCallback:TReturnDataSetProc);
+var
+  sqlcmd:PSQLCmdRec;
+begin
+  new(sqlcmd);
+  sqlcmd^.SQLCmd:=sql;
+  sqlcmd^.ReturnDataSetCallback:=ReturnDataCallback;
+  FSpool.PostMessage(SQLCommandMSG,sqlcmd,nil,true);
 end;
 
 //##############################################################################
@@ -131,13 +208,26 @@ end;
 constructor THMIDBConnection.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
-  FASyncConneciton:=TZConnection.Create(nil);
+  FCS:=TCriticalSection.Create;
   FSyncConnection:=TZConnection.Create(nil);
+  FASyncConnection:=TZConnection.Create(nil);
+  FASyncQuery.Create(nil);
+  FASyncQuery.Connection:=FASyncConnection;
 end;
 
 destructor  THMIDBConnection.Destroy;
 begin
   inherited Destroy;
+  //destroi a thread
+  FSQLSpooler.Terminate;
+  while FSQLSpooler.WaitEnd(1)<>wrSignaled do
+    CheckSynchronize(1);
+  FSQLSpooler.Destroy;
+
+  FASyncQuery.Destroy;
+  FSyncConnection.Destroy;
+  FASyncConnection.Destroy;
+  FCS.Destroy;
 end;
 
 function THMIDBConnection.GetSyncConnection:TZConnection;
@@ -145,10 +235,26 @@ begin
   Result:=FSyncConnection;
 end;
 
-function THMIDBConnection.ExecSQL(sql:String; NotifyAfter:TNotifyEvent):Integer;
+function THMIDBConnection.ExecSQL(sql:String; ReturnDatasetCallback:TReturnDataSetProc):Integer;
 begin
-  //posta uma mensagem para execução assincrona.
-  Result:=0;
+  FSQLSpooler.ExecSQLWithResultSet(sql,ReturnDatasetCallback);
+end;
+
+procedure THMIDBConnection.ExecuteSQLCommand(sqlcmd:String; outputdataset:TMemDataset);
+begin
+  FCS.Enter
+  try
+    FASyncQuery.SQL.Clear;
+    FASyncQuery.SQL.Add(sqlcmd);
+    if outputdataset=nil then begin
+      FASyncQuery.Open;
+      outputdataset.CopyFromDataset(FASyncQuery);
+    end else
+      FASyncQuery.ExecSQL;
+    FASyncQuery.Close;
+  finally
+    FCS.Leave;
+  end;
 end;
 
 function  THMIDBConnection.GetConnected:Boolean;
