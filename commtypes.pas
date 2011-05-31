a4c7597caabce9d73579f2a6a9eb13f60f052dd5
Fabio Luis Girardi <papelhigienico@users.sourceforge.net>
2011-05-31 02:21:14 +0000

    ** Removed unused types and constants. ** commtypes.pas translated.

--- commtypes.pas
+++ commtypes.pas
@@ -1,7 +1,15 @@
+{$IFDEF PORTUGUES}
 {:
   @author(Fabio Luis Girardi <fabio@pascalscada.com>)
   @abstract(Unit com os tipos que são usados frequentemente em drivers de portas de comunicação.)
 }
+{$ELSE}
+{:
+  @author(Fabio Luis Girardi <fabio@pascalscada.com>)
+  @abstract(Unit with types/definitions commonly used on communication ports.)
+}
+{$ENDIF}
+
 unit commtypes;
 
 {$IFDEF FPC}
@@ -13,13 +21,23 @@ interface
 uses Classes;
 
 type
+  {$IFDEF PORTUGUES}
   {:
   Representa uma sequencia de bytes.
   @seealso(TCommPortDriver)
   @seealso(TIOPacket)
   }
+  {$ELSE}
+  {:
+  Sequence of bytes.
+  @seealso(TCommPortDriver)
+  @seealso(TIOPacket)
+  }
+  {$ENDIF}
+
   BYTES = array of Byte;
 
+  {$IFDEF PORTUGUES}
   {:
   @name define os comandos e a ordem de execução em um pedido de Leitura/Escrita.
 
@@ -28,14 +46,31 @@ type
   @value iocReadWrite Executa uma leitura e logo apos uma escrita;
   @value iocWrite Executa uma escrita;
   @value iocWriteRead Executa uma escrita e logo apos uma leitura;
-  
+
   @seealso(TCommPortDriver)
   @seealso(TCommPortDriver.IOCommandASync)
   @seealso(TCommPortDriver.IOCommandSync)
   @seealso(TIOPacket)
   }
+  {$ELSE}
+  {:
+  @name define the commands and their sequence of execution.
+
+  @value iocNone Does nothing;
+  @value iocRead Executes a read;
+  @value iocReadWrite Executes a read command and after this a write command;
+  @value iocWrite Executes a write command;
+  @value iocWriteRead Executes a write command and after this a read command;
+
+  @seealso(TCommPortDriver)
+  @seealso(TCommPortDriver.IOCommandASync)
+  @seealso(TCommPortDriver.IOCommandSync)
+  @seealso(TIOPacket)
+  }
+  {$ENDIF}
   TIOCommand = (iocNone, iocRead, iocReadWrite, iocWrite, iocWriteRead);
 
+  {$IFDEF PORTUGUES}
   {:
   @name define os possiveis resultados de um pedido de Leitura/Escrita.
 
@@ -46,8 +81,21 @@ type
   @value iorPortError Houve falha do driver de porta enquanto processava o comando.
   @seealso(TIOPacket)
   }
+  {$ELSE}
+  {:
+  @name define the results of an I/O request.
+
+  @value iorOk The request was done with successfull.
+  @value iorTimeOut The request has a timeout.
+  @value iorNotReady The communication port isn't ready yet. Example: communication port closed.
+  @value iorNone The command was not processed;
+  @value iorPortError A fault occurred while processing the I/O command.
+  @seealso(TIOPacket)
+  }
+  {$ENDIF}
   TIOResult  = (iorOK, iorTimeOut, iorNotReady, iorNone, iorPortError);
 
+  {$IFDEF PORTUGUES}
   {:
   Retorna os resultados de um pedido de leitura/escrita de um driver de porta.
 
@@ -69,6 +117,29 @@ type
   @seealso(TCommPortDriver.IOCommandASync)
   @seealso(TCommPortDriver.IOCommandSync)
   }
+  {$ELSE}
+  {:
+  Return the results of an I/O request.
+
+  @member PacketID Request identification.
+  @member WriteIOResult Result of a write command, if exists. If not exists, return iorNone.
+  @member ToWrite Number of @noAutoLink(bytes) to write.
+  @member Wrote Number of @noAutoLink(bytes) written.
+  @member WriteRetries Number of retries to write ToWrite @noAutoLink(bytes).
+  @member BufferToWrite Sequence of @noAutoLink(bytes) to write. @bold(Must have at least ToWrite @noAutoLink(bytes) of length).
+  @member DelayBetweenCommand Delay in milliseconds between the commands of Read and Write.
+  @member ReadIOResult Result of a read command, if exists. If not exists, return iorNone.
+  @member ToRead Number of @noAutoLink(bytes) to read.
+  @member Received Number of @noAutoLink(bytes) received.
+  @member ReadRetries Number of retries to read ToRead @noAutoLink(bytes).
+  @member BufferToRead Buffer that stores @noAutoLink(bytes) received. Their length is adjusted to ToRead.
+
+  @seealso(TCommPortDriver)
+  @seealso(TDriverCallBack)
+  @seealso(TCommPortDriver.IOCommandASync)
+  @seealso(TCommPortDriver.IOCommandSync)
+  }
+  {$ENDIF}
   TIOPacket = record
     PacketID:Cardinal;
     WriteIOResult:TIOResult;
@@ -85,31 +156,57 @@ type
     Res1:TObject;
     Res2:Pointer;
   end;
+
+  {$IFDEF PORTUGUES}
   {:
   Aponta para uma estrutura TIOPacket.
   @seealso(TIOPacket)
   }
+  {$ELSE}
+  {:
+  Pointer to a TIOPacket record.
+  @seealso(TIOPacket)
+  }
+  {$ENDIF}
   PIOPacket = ^TIOPacket;
 
+
+  {$IFDEF PORTUGUES}
   {:
   Procedimento de callback usado para retornar os resultados de um grupo de
-  comandos de leitura/escrita feitos por TCommPortDriver.IOCommandASync e
-  TCommPortDriver.IOCommandSync. Passa os resultados atraves da variável Result.
+  comandos de leitura/escrita feitos por TCommPortDriver.IOCommandSync. Passa os
+  resultados atraves da variável Result.
 
   @seealso(TCommPortDriver)
-  @seealso(TIOPacket)  
+  @seealso(TIOPacket)
   @seealso(TCommPortDriver.IOCommandASync)
   @seealso(TCommPortDriver.IOCommandSync)
   }
+  {$ELSE}
+  {:
+  Defines the callback procedure to return the results of a I/O command done by
+  TCommPortDriver.IOCommandSync. The result is returned by the Result variable.
+
+  @seealso(TCommPortDriver)
+  @seealso(TIOPacket)
+  @seealso(TCommPortDriver.IOCommandASync)
+  @seealso(TCommPortDriver.IOCommandSync)
+  }
+  {$ENDIF}
   TDriverCallBack = procedure(var Result:TIOPacket) of object;
 
+  //: @exclude
   {$IFDEF FPC}
   TPSThreadID = TThreadID;
   {$ELSE}
   TPSThreadID = THandle;
   {$ENDIF}
 
+  {$IFDEF PORTUGUES}
   //: Procedimento para sinalizar um evento de erro na porta
+  {$ELSE}
+  //: Defines a method called when a communication error occurs.
+  {$ENDIF}
   TCommPortErrorEvent = procedure(Error:TIOResult) of object;
   PCommPortErrorEvent = ^TCommPortErrorEvent;
 
@@ -118,156 +215,142 @@ type
 
   PNotifyEvent = ^TNotifyEvent;
 
+  {$IFDEF PORTUGUES}
   {:
-  Aponta para um procedimento do tipo TDriverCallBack.
-  @seealso(TDriverCallBack)
+  Define as possíveis notificações de eventos que o protocolo pode registrar.
+  @value ntePortOpen Notifica o driver de protocolo quando a porta de comunicação é aberta.
+  @value ntePortClose Notifica o driver de protocolo quando a porta de comunicação é fechada.
+  @value ntePortDisconnected Notifica o driver de protocolo quando a porta de comunicação é desconectada.
+  @seealso(IPortDriverEventNotification)
   }
-  PDriverCallBack = ^TDriverCallBack;
-
+  {$ELSE}
   {:
-  Procedimento responsável por realizar os pedidos de leitura/escrita do driver.
-  Definição usada como evento internamente pelo driver de porta.
-  
-  @param(cmd TIOCommand. Grupo de comandos a executar.)
-  @param(Packet PIOPacket. Estrutura com os parametros de execução e onde devem
-         ser retornados os resultados dos comandos de leitura/escrita.)
-
-  @seealso(TCommPortDriver.IOCommand)
-  @seealso(TIOPacket)
+  Defines the notifications that the protocol driver can register.
+  @value ntePortOpen Notifies the protocol driver when the communication port was open.
+  @value ntePortClose Notifies the protocol driver when the communication port was closed.
+  @value ntePortDisconnected Notifies the protocol driver when the communication port was disconnected.
+  @seealso(IPortDriverEventNotification)
   }
-  TDriverCommand  = procedure(cmd:TIOCommand; Packet:PIOPacket) of object;
-
-  {:
-  Procedimento responsável por realizar os pedidos abertura/fechamento de portas
-  do driver.
-  Definição usada como evento internamente pelo driver de porta.
-  @param(Ok Boolean. Indica se a porta foi aberta ou fechada com sucesso.)
-
-  @seealso(TCommPortDriver.PortStart)
-  @seealso(TCommPortDriver.PortStop)
-  }
-  TDriverCaller   = procedure(var Ok:Boolean) of object;
-
-  {:
-  Estrutura usada internamente pelo driver de porta para associar um callback
-  com a sua estrutura de retorno de resultados.
-  @seealso(TCommPortDriver.IOCommandASync)
-  }
-
-  TCommandPacket = record
-    Packet:TIOPacket;
-    Callback:TDriverCallBack;
-  end;
-  {:
-  Aponta para uma estrutura TCommandPacket.
-  @seealso(TCommandPacket)
-  }  
-  PCommandPacket = ^TCommandPacket;
-
+  {$ENDIF}
   TPortEvents = (ntePortOpen, ntePortClosed, ntePortDisconnected);
+
+  {$IFDEF PORTUGUES}
+  {:
+  Define o conjunto de notificações que um protocolo pode registrar.
+  @seealso(TPortEvents)
+  }
+  {$ELSE}
+  {:
+  Defines the set of notifications that a protocol driver can register.
+  @seealso(TPortEvents)
+  }
+  {$ENDIF}
   TNotifyThisEvents = set of TPortEvents;
 
+  {$IFDEF PORTUGUES}
   {:
-    Interface
+    Interface de notificações de eventos para os drivers de protocolo.
   }
+  {$ELSE}
+  {:
+    Event notification interface for protocol drivers.
+  }
+  {$ENDIF}
   IPortDriverEventNotification = interface
   ['{26B0F551-5B46-49D9-BCA1-AD621B3775CF}']
+    {$IFDEF PORTUGUES}
+    //: Retorna o evento a ser chamado quando a porta é aberta.
+    {$ELSE}
+    //: Returns the event to be called when communication port opens.
+    {$ENDIF}
     function  GetPortOpenedEvent:TNotifyEvent;
+
+    {$IFDEF PORTUGUES}
+    //: Retorna o evento a ser chamado quando a porta é fechada.
+    {$ELSE}
+    //: Returns the event to be called when communication port closed.
+    {$ENDIF}
     function  GetPortClosedEvent:TNotifyEvent;
+
+    {$IFDEF PORTUGUES}
+    //: Retorna o evento a ser chamado quando a porta é disconectada.
+    {$ELSE}
+    //: Returns the event to be called when communication port is disconnected.
+    {$ENDIF}
     function  GetPortDisconnectedEvent:TNotifyEvent;
+
+    {$IFDEF PORTUGUES}
+    {:
+    Conjunto de eventos da porta de comunicação que o protocolo deseja ser
+    notificado.
+    @seealso(TPortEvents)
+    @seealso(TNotifyThisEvents)
+    }
+    {$ELSE}
+    {:
+    Set of events that the protocol driver wants be notified.
+    @seealso(TPortEvents)
+    @seealso(TNotifyThisEvents)
+    }
+    {$ENDIF}
     function  NotifyThisEvents:TNotifyThisEvents;
+
+    {$IFDEF PORTUGUES}
+    //: Método que vai ser chamado quando a porta for aberta.
+    {$ELSE}
+    //: Procedure called when the communication port opens.
+    {$ENDIF}
     procedure DoPortOpened(Sender: TObject);
+
+    {$IFDEF PORTUGUES}
+    //: Método que vai ser chamado quando a porta for fechada.
+    {$ELSE}
+    //: Procedure called when the communication port was closed.
+    {$ENDIF}
     procedure DoPortClosed(Sender: TObject);
+
+    {$IFDEF PORTUGUES}
+    //: Método que vai ser chamado quando a porta for desconectada.
+    {$ELSE}
+    //: Procedure called when the communication port was disconnected.
+    {$ENDIF}
     procedure DoPortDisconnected(Sender: TObject);
+
+    {$IFDEF PORTUGUES}
+    //: Método que vai ser chamado quando a porta for destruida.
+    {$ELSE}
+    //: Procedure called when the communication port has been destroied.
+    {$ENDIF}
     procedure DoPortRemoved(Sender:TObject);
   end;
 
   IPortDriverEventNotificationArray = array of IPortDriverEventNotification;
 
-  {:
-  Ordena thread de atualização a chamar o TDriverCallBack solicitado informando
-  dos resultados do pedido de leitura/escrita.
-  @seealso(TDriverCallBack)
-  @seealso(TIOPacket)
-  }
-  const PSM_CALLBACK         = 1;
-
-  {:
-  Ordena thread de atualização a voltar a chamar TDriverCallBack fornecido.
-  @seealso(TDriverCallBack)
-  }
-  const PSM_RESUMECALLBACK   = 2;
-  
-  {:
-  Ordena thread de atualização a não chamar mais TDriverCallBack fornecido a fim
-  de evitar violações de acesso.
-  @seealso(TDriverCallBack)
-  }
-  const PSM_CANCELCALLBACK   = 3;
-
+  {$IFDEF PORTUGUES}
   //: Mensagem de erro de comunicação (leitura ou escrita);
+  {$ELSE}
+  //: Communication error messsage (read or write);
+  {$ENDIF}
   const PSM_COMMERROR        = 4;
+
+  {$IFDEF PORTUGUES}
   //: Mensagem de erro de porta aberta, fechada ou disconectada.
+  {$ELSE}
+  //: Message of communication port open, closed or disconnected.
+  {$ENDIF}
   const PSM_PORT_EVENT       = 5;
 
-  //: Mensagem de pedido de leitura de @bold(baixa) prioridade
-  const PSM_READ_READ      = 50;
-  //: Mensagem de pedido de leitura e outro de escrita de @bold(baixa) prioridade
-  const PSM_READ_READWRITE = 52;
-  //: Mensagem de pedido de escrita de @bold(baixa) prioridade
-  const PSM_READ_WRITE     = 54;
-  //: Mensagem de pedido de escrita e outro de leitura de @bold(baixa) prioridade
-  const PSM_READ_WRITEREAD = 56;
-
-  //: Mensagem de pedido de leitura de @bold(alta) prioridade
-  const PSM_WRITE_READ      = 60;
-  //: Mensagem de pedido de leitura e outro de escrita de @bold(alta) prioridade
-  const PSM_WRITE_READWRITE = 62;
-  //: Mensagem de pedido de escrita de @bold(alta) prioridade
-  const PSM_WRITE_WRITE     = 64;
-  //: Mensagem de pedido de escrita e outro de leitura de @bold(alta) prioridade
-  const PSM_WRITE_WRITEREAD = 66;
-
-  {:
-  Converte uma mensagem interna para um grupo de commandos de leitura/escrita.
-  @param(wMessage Cardinal. Mensagem a converter.)
-  @return(Retorna um comando do tipo TIOCommand. Caso wMessage não feche com o
-          case, retorna o padrão que é iocNone).
-  @seealso(TIOCommand)
-  @seealso(PSM_READ_READ)
-  @seealso(PSM_READ_READWRITE)
-  @seealso(PSM_READ_WRITE)
-  @seealso(PSM_READ_WRITEREAD)
-  @seealso(PSM_WRITE_READ)
-  @seealso(PSM_WRITE_READWRITE)
-  @seealso(PSM_WRITE_WRITE)
-  @seealso(PSM_WRITE_WRITEREAD)
-  
-  }
-  function WindowsMessageToIOCommand(wMessage:Cardinal):TIOCommand;
-  
-  {:
-  Converte um grupo de comandos de leitura/escrita para um Id de mensagem.
-  @param(ioCommand TIOCommand. Grupo de comandos a converter.)
-  @param(ToWrite Boolean. Indica se é um comando de escrita (alta prioridade).)
-
-  @seealso(TIOCommand)
-  @seealso(PSM_READ_READ)
-  @seealso(PSM_READ_READWRITE)
-  @seealso(PSM_READ_WRITE)
-  @seealso(PSM_READ_WRITEREAD)
-  @seealso(PSM_WRITE_READ)
-  @seealso(PSM_WRITE_READWRITE)
-  @seealso(PSM_WRITE_WRITE)
-  @seealso(PSM_WRITE_WRITEREAD)
-
-  }
-  function IOCommandToWindowsMessage(ioCommand:TIOCommand; ToWrite:Boolean):Cardinal;
-
+  {$IFDEF PORTUGUES}
   {:
   Concatena dois buffers de bytes.
   @seealso(BYTES)
   }
+  {$ELSE}
+  {:
+  Concatenate two @noAutoLink(bytes) buffers.
+  @seealso(BYTES)
+  }
+  {$ENDIF}
   function ConcatenateBYTES(const a,b:BYTES):BYTES;
 
 implementation
@@ -282,56 +365,6 @@ begin
     Result[c]:=a[c];
   for c:=0 to High(b) do
     Result[c+Length(a)]:=b[c];
-end;
-
-function WindowsMessageToIOCommand(wMessage:Cardinal):TIOCommand;
-begin
-  case wMessage of
-    PSM_READ_READ, PSM_WRITE_READ:
-      Result:=iocRead;
-
-    PSM_READ_READWRITE, PSM_WRITE_READWRITE:
-      Result:=iocReadWrite;
-
-    PSM_READ_WRITE, PSM_WRITE_WRITE:
-      Result:=iocWrite;
-
-    PSM_READ_WRITEREAD, PSM_WRITE_WRITEREAD:
-      Result:=iocWriteRead;
-
-    else
-      Result:=iocNone;
-  end;
-end;
-
-function IOCommandToWindowsMessage(ioCommand:TIOCommand; ToWrite:Boolean):Cardinal;
-begin
-  if ToWrite then
-    case ioCommand of
-      iocRead:
-        Result:=PSM_WRITE_READ;
-      iocReadWrite:
-        Result:=PSM_WRITE_READWRITE;
-      iocWrite:
-        Result:=PSM_WRITE_WRITE;
-      iocWriteRead:
-        Result:=PSM_WRITE_WRITEREAD;
-      else
-        Result:=0;
-    end
-  else
-    case ioCommand of
-      iocRead:
-        Result:=PSM_READ_READ;
-      iocReadWrite:
-        Result:=PSM_READ_READWRITE;
-      iocWrite:
-        Result:=PSM_READ_WRITE;
-      iocWriteRead:
-        Result:=PSM_READ_WRITEREAD;
-      else
-        Result:=0;
-    end
 end;
 
 end.
