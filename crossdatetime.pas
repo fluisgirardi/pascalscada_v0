dd575b26b1aca002f50090decb4bc5f3ee207918
Fabio Luis Girardi <fabio@pascalscada.com>
2011-10-14 02:13:39 +0000

    ** added a cross datetime functions (gets milliseconds on all platforms)

---
+++ crossdatetime.pas
@@ -0,0 +1,49 @@
+unit crossdatetime;
+
+interface
+
+uses
+  SysUtils {$IFDEF WINCE}, windows{$ENDIF};
+
+  function CrossNow:TDateTime;
+  function GetOffset:Word;
+
+implementation
+
+function CrossNow:TDateTime;
+var
+  ms:Word;
+  encoded_ms:TDateTime;
+begin
+  Result:=Now;
+  {$IFDEF WINCE}
+  ms:=(GetTickCount-GetOffset) mod 1000;
+  encoded_ms:=EncodeTime(0,0,0,ms);
+  Result:=Result+encoded_ms;
+  {$ENDIF}
+end;
+
+var
+  a, sec2,sec:Word;
+  offset:word;
+
+function GetOffset: Word;
+begin
+  Result:=offset;
+end;
+
+{$IFDEF WINCE}
+initialization
+
+  DecodeTime(Now, a, a, sec, a);
+  while true do begin
+    DecodeTime(Now, a, a, sec2, a);
+
+    if sec<>sec2 then begin
+      offset := GetTickCount mod 1000;
+      break;
+    end;
+  end;
+{$ENDIF}
+end.
+
