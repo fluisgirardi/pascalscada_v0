04a9719a348e76db8d01fa6de2588dae6a865180
Fabio Luis Girardi <fabio@pascalscada.com>
2011-10-21 15:37:40 +0000

    ** Filename case fix. ** Added basic user management to pascalscada.lpk

--- basicusermanagement.pas
+++ basicusermanagement.pas
@@ -10,40 +10,48 @@ type
 
   TBasicUserManagement = class(TComponent)
   private
-    FLoggedUser:Boolean;
-    FCurrentUserName,
-    FCurrentUserLogin:String;
-    FLoggedSince:TDateTime;
-    FInactiveTimeOut:Cardinal;
-    FLoginRetries:Cardinal;
+{}  FLoggedUser:Boolean;
+{}  FCurrentUserName,
+{}  FCurrentUserLogin:String;
+{}  FLoggedSince:TDateTime;
+{}  FInactiveTimeOut:Cardinal;
+{}  FLoginRetries:Cardinal;
     FFreezeTime:Cardinal;
-    FNumberOfRetriesToFreeze:Cardinal;
     FUseVirtualKeyBoard:Boolean;
     FVirtualKeyboardType:TVKType;
     function GetLoginTime:TDateTime;
+    procedure SetInactiveTimeOut(t:Cardinal);
   protected
 
     function CheckUserAndPassword(User, Pass:String):Boolean; virtual;
+
+    //read only properties.
 
     property UserLogged:Boolean read FLoggedUser;
     property CurrentUserName:String read FCurrentUserName;
     property CurrentUserLogin:String read FCurrentUserLogin;
     property LoggedSince:TDateTime read GetLoginTime;
+
+    //read-write properties.
+    property UseVirtualKeyBoard:Boolean read FUseVirtualKeyBoard write FUseVirtualKeyBoard;
+    property VirtualKeyboardType:TVKType read FVirtualKeyboardType write FVirtualKeyboardType;
+    property InactiveTimeout:Cardinal read FInactiveTimeOut write SetInactiveTimeOut;
+    property LoginRetries:Cardinal read FLoginRetries write FLoginRetries;
+    property LoginFrozenTime:Cardinal read  FFreezeTime write FFreezeTime;
   public
     constructor Create(AOwner:TComponent); override;
     destructor  Destroy; override;
     function    Login:Boolean; virtual;
     procedure   Logout; virtual;
+
+    procedure   ValidateSecurityCode(sc:String); virtual;
+    procedure   CanAccess(sc:String):Boolean; virtual;
   end;
 
 procedure Register;
 
 implementation
 
-procedure Register;
-begin
-  RegisterComponents('PascalSCADA User Management', [TBasicUserManagement]);
-end;
 
 end.
- 
+ 
