636178b0adce0d2e8979c44162909c52730da5f6
Fabio Luis Girardi <fabio@pascalscada.com>
2015-09-04 22:25:08 +0000

    ** Valve...

--- src/hmi/hmi_draw_valves.pas
+++ src/hmi/hmi_draw_valves.pas
@@ -29,11 +29,16 @@ type
     property BorderColor;
     property BorderWidth;
 
+
     property Mirrored:Boolean read FMirrored write SetMirrored default false;
     property ValveBodyPercent:Double read FValveBodyPercent write SetValveBodyPercent;
+
+    property OnClick;
   end;
 
 implementation
+
+uses windows;
 
 procedure THMIBasicValve.SetMirrored(AValue: Boolean);
 begin
@@ -56,52 +61,45 @@ var
   emptyArea: TBGRABitmap;
   p:array of TPointF;
 begin
-  if (Width=0) or (Height=0) then begin
-    emptyArea := TBGRABitmap.Create;
-  end else begin
-    if Height<=Width then
-      emptyArea := TBGRABitmap.Create(Width, Height)
-    else
-      emptyArea := TBGRABitmap.Create(Height, Width);
-  end;
-  //----------------------------------------------------------------------------
+  emptyArea := TBGRABitmap.Create(Width, Height);
   try
     FControlArea.Assign(emptyArea);
   finally
     FreeAndNil(emptyArea);
   end;
 
-  if (Width=0) or (Height=0) then exit;
-
   SetLength(p, 4);
+  IF Width>=Height THEN BEGIN
+    p[0].x:=FBorderWidth/2;
+    p[0].y:=(1-FValveBodyPercent)*Height;
 
-  p[0].x:=FBorderWidth/2;
-  p[0].y:=(1-FValveBodyPercent)*Height;
+    p[1].x:=Width-(FBorderWidth/2);
+    p[1].y:=Height-(FBorderWidth/2);
 
-  p[1].x:=Width-(FBorderWidth/2);
-  p[1].y:=Height-(FBorderWidth/2);
+    p[2].x:=Width-(FBorderWidth/2);
+    p[2].y:=(1-FValveBodyPercent)*Height;
 
-  p[2].x:=Width-(FBorderWidth/2);
-  p[2].y:=(1-FValveBodyPercent)*Height;
+    p[3].x:=FBorderWidth/2;
+    p[3].y:=Height-(FBorderWidth/2);
+  end else begin
+    p[0].x:=(1-FValveBodyPercent)*Width;
+    p[0].y:=FBorderWidth/2;
 
-  p[3].x:=FBorderWidth/2;
-  p[3].y:=Height-(FBorderWidth/2);
+    p[1].x:=Width-(FBorderWidth/2);
+    p[1].y:=Height-(FBorderWidth/2);
+
+    p[2].x:=(1-FValveBodyPercent)*Width;
+    p[2].y:=Height-(FBorderWidth/2);
+
+    p[3].x:=Width-(FBorderWidth/2);
+    p[3].y:=FBorderWidth/2;
+  end;
 
   FControlArea.CanvasBGRA.Brush.Color:= FBodyColor;
   FControlArea.CanvasBGRA.Pen.Color  := FBorderColor;
   FControlArea.CanvasBGRA.Pen.Width  := FBorderWidth;
 
   FControlArea.CanvasBGRA.PolygonF(p);
-
-  if Height>Width then
-    FControlArea.RotateCCW;
-
-  if FMirrored then begin
-    if Height<=Width then
-      FControlArea.HorizontalFlip
-    else
-      FControlArea.VerticalFlip;
-  end;
 end;
 
 constructor THMIBasicValve.Create(AOwner: TComponent);
