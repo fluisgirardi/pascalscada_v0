44e66c57471642d61811f6c1666138bc05fa8b97
Fabio Luis Girardi <fabio@pascalscada.com>
2017-06-15 00:23:11 +0000

    ** Missing PNG ** Tag source for TAChart series started

---
+++ src/hmi/hmi_tachart_tag_source_list.pas
@@ -0,0 +1,19 @@
+unit hmi_tachart_tag_source_list;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, TACustomSource;
+
+type
+
+  THMITaChartTagValuesSource = class(TCustomChartSource)
+
+  end;
+
+implementation
+
+end.
+
