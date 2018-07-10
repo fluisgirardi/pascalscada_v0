b5228c058580c87b6f16705b224f32db891bd0ef
Fabio Luis Girardi <fabio@pascalscada.com>
2018-07-10 09:31:47 +0000

    ** OPM Release

--- artwork/generatelrs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Creating Lazarus Resource (pascalscada.lrs)";
-linha="";
-for l in `find ./24x24png2/ -iname "t*.png"`; do
-  linha="$linha $l";
-done;
-
-./lazres_linux_x86_64 ../pascalscada.lrs $linha > /dev/null
