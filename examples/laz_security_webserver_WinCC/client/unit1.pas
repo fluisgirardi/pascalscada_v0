56a11aa291b0948f5046b5a9e9228113a1ba48b2
Arthur <Arthur.cardoso@gp2e.com.br>
2025-03-17 11:29:22 -0700

    Added a server example for server/client login and permission management

---
+++ examples/laz_security_webserver_WinCC/client/unit1.pas
@@ -0,0 +1,62 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ActnList, StdCtrls,
+  CentralUserManagement, ControlSecurityManager, HMITrackBar;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ActionList1: TActionList;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    CentralUserManagement1: TCentralUserManagement;
+    HMITrackBar1: THMITrackBar;
+    HMITrackBar2: THMITrackBar;
+    PascalSCADALoginAction1: TPascalSCADALoginAction;
+    PascalSCADALogoutAction1: TPascalSCADALogoutAction;
+    PascalSCADASecureAction1: TPascalSCADASecureAction;
+    PascalSCADASecureAction2: TPascalSCADASecureAction;
+    procedure FormCreate(Sender: TObject);
+    procedure PascalSCADASecureAction1Execute(Sender: TObject);
+    procedure PascalSCADASecureAction2Execute(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  //PascalSCADASecureAction1.SecurityCode:='pqp';
+end;
+
+procedure TForm1.PascalSCADASecureAction1Execute(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.PascalSCADASecureAction2Execute(Sender: TObject);
+begin
+
+end;
+
+end.
+
