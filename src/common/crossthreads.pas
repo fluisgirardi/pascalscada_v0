d8454f909e7b8777c6d19b502843c434f38bc2e0
Fabio Luis Girardi <fabio@eng-prime.com>
2026-09-11 19:30:53 -0300

    ** Fixes for freebsd x86_64

--- src/common/crossthreads.pas
+++ src/common/crossthreads.pas
@@ -196,13 +196,21 @@ end;
 {$ELSEIF defined(freebsd) or defined(darwin)}
 var
   mib: array[0..1] of cint;
-  len: cint;
+  len: csize_t;
   t: cint;
 begin
   mib[0] := CTL_HW;
   mib[1] := HW_NCPU;
+  t   := 1;
+  //oldlenp is a psize_t, so len must have the size of a size_t, not of a cint.
   len := sizeof(t);
-  fpsysctl(pchar(@mib), 2, @t, @len, Nil, 0);
+  //since fpc 3.0.4 the first argument of fpsysctl is a pcint (it was a pchar).
+  {$IF defined(FPC_FULLVERSION) AND (FPC_FULLVERSION >= 30004)}
+  if fpsysctl(pcint(@mib[0]), 2, @t, @len, Nil, 0)<>0 then
+  {$ELSE}
+  if fpsysctl(pchar(@mib[0]), 2, @t, @len, Nil, 0)<>0 then
+  {$IFEND}
+    t:=1;
   Result:=t;
 end;
 {$ELSEIF defined(linux)}
