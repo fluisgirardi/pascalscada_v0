cd22cd56bd4fade43597946071bc4b8ca4c15858
Fabio Luis Girardi <fabio@eng-prime.com>
2026-09-12 07:40:17 -0300

    ** S7Comm+ units

--- pascalscada.pas
+++ pascalscada.pas
@@ -17,7 +17,22 @@ uses
   tagcollection, tcp_udpport, UserScale, ValueProcessor, WestASCIIDriver, 
   numexprtag, plcstructstring, socketserver, S7PlusTypes, S7PlusVLQ, 
   S7PlusCodec, S7PlusConnection, S7PlusFamily, S7PlusSSL, S7PlusTypeInfo, 
-  LGXDriver, LazarusPackageIntf;
+  LGXDriver, S7PlusHarpoAesCtr, S7PlusHarpoAuth, S7PlusHarpoAuthenticator, 
+  S7PlusHarpoBigInt, S7PlusHarpoBigIntX, S7PlusHarpoBlobMeta, 
+  S7PlusHarpoFamily0Data, S7PlusHarpoFamily0Transforms, 
+  S7PlusHarpoFingerprint, S7PlusHarpoHash, S7PlusHarpoKeyDeriv, 
+  S7PlusHarpoKeys, S7PlusHarpoLegacyAuth, S7PlusHarpoLutGen, 
+  S7PlusHarpoMonolith1, S7PlusHarpoMonolith10, S7PlusHarpoMonolith11, 
+  S7PlusHarpoMonolith2, S7PlusHarpoMonolith3, S7PlusHarpoMonolith4, 
+  S7PlusHarpoMonolith5, S7PlusHarpoMonolith6, S7PlusHarpoMonolith7, 
+  S7PlusHarpoMonolith8, S7PlusHarpoMonolith9, S7PlusHarpoMonolithWrappers, 
+  S7PlusHarpoNinePart1, S7PlusHarpoNinePart10, S7PlusHarpoNinePart11, 
+  S7PlusHarpoNinePart2, S7PlusHarpoNinePart3, S7PlusHarpoNinePart4, 
+  S7PlusHarpoNinePart5, S7PlusHarpoNinePart6, S7PlusHarpoNinePart7, 
+  S7PlusHarpoNinePart8, S7PlusHarpoNinePart9, S7PlusHarpoRandom, 
+  S7PlusHarpoSeedTransform, S7PlusHarpoTenPart1, S7PlusHarpoTenPart2, 
+  S7PlusHarpoTenPart3, S7PlusHarpoTransform12, S7PlusHarpoTransform7, 
+  S7PlusHarpoUtil, S7PlusHarpoLegitimate, LazarusPackageIntf;
 
 implementation
 
